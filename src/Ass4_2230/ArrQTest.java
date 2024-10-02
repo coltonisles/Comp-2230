e2b409be09d3089a1724b4a9d1a45a8e7f9e16b2
Colton Isles <coltonisles@gmail.com>
2024-10-02 12:38:30 -0700

    Made test class for array queue.

--- src/Ass4_2230/ArrQTest.java
+++ src/Ass4_2230/ArrQTest.java
@@ -1,36 +1,66 @@
 package Ass4_2230;
 
+import Ass2_2230.exceptions.EmptyCollectionException;
+
+import java.util.Arrays;
+
 public class ArrQTest {
-    public static void main(String[] args){
-        CircularArrayQueue<Integer> arrQ = new CircularArrayQueue<>(5);
+    public static void main(String[] args) {
+        CircularArrayQueue<Integer> arrq = new CircularArrayQueue<>(5);
 
-        arrQ.enqueue(1);
-        arrQ.enqueue(2);
-        arrQ.enqueue(3);
-        arrQ.enqueue(4);
-        arrQ.enqueue(5);
-        System.out.println(arrQ.toString());
-        System.out.println("next: " + arrQ.first());
-        System.out.println("size: " + arrQ.size());
-        arrQ.enqueue(6);
-        System.out.println(arrQ.toString());
-        System.out.println("next: " + arrQ.first());
-        System.out.println("size: " + arrQ.size());
-        arrQ.dequeue();
-        arrQ.dequeue();
-        arrQ.dequeue();
-        arrQ.dequeue();
-        System.out.println(arrQ.toString());
-        System.out.println("next: " + arrQ.first());
-        System.out.println("size: " + arrQ.size());
-        arrQ.enqueue(1);
-        arrQ.enqueue(2);
-        arrQ.enqueue(3);
-        arrQ.enqueue(4);
-        arrQ.enqueue(5);
-        System.out.println(arrQ.toString());
-        System.out.println("next: " + arrQ.first());
-        System.out.println("size: " + arrQ.size());
+        // Initialization with null values and capacity 5
+        System.out.println("Current Queue: Front <- " + arrq.toString() + " <- Rear");
 
+        // Populate the queue to fill the initial capacity
+        for (int i = 1; i <= 5; i++) {
+            arrq.enqueue(i);
+        }
+        System.out.println("Current Queue: Front <- " + arrq.toString() + " <- Rear");
+
+        // Test isEmpty and size
+        System.out.println("Is the queue empty: " + arrq.isEmpty());
+        System.out.println("Queue size: " + arrq.size());
+
+        // Test expandCapacity() method
+        System.out.println("-----expandCapacity() Test-----");
+        for (int i = 6; i <= 10; i++) {
+            arrq.enqueue(i);
+        }
+        System.out.println("Current Queue: Front <- " + arrq.toString() + " <- Rear");
+
+        // Test dequeue() and first() methods
+        System.out.println("-----dequeue() & first() Test-----");
+        System.out.println("First Value before dequeue: " + arrq.first());
+        System.out.println("Queue before dequeue: Front <- " + arrq.toString() + " <- Rear");
+        for (int i = 0; i < 5; i++) {
+            arrq.dequeue();
+        }
+        System.out.println("First Value after dequeue half the queue: " + arrq.first());
+        System.out.println("Queue after dequeue half the queue: Front <- " + arrq.toString() + " <- Rear");
+        for (int i = 0; i < 5; i++) {
+            arrq.dequeue();
+        }
+        System.out.println("First Value after dequeue the entire queue: " + arrq.first());
+        System.out.println("Queue after dequeue the entire queue: Front <- " + arrq.toString() + " <- Rear");
+
+        // Test first() with empty queue
+        System.out.println("-----first() with empty queue test-----");
+        try {
+            arrq.first();
+        } catch (EmptyCollectionException ece) {
+            System.out.println("first() throws empty collection exception correctly");
+        }
+
+        // Test dequeue() with empty queue
+        System.out.println("-----dequeue() with empty queue test-----");
+        try {
+            arrq.dequeue();
+        } catch (EmptyCollectionException ece) {
+            System.out.println("dequeue() throws empty collection exception correctly");
+        }
+
+        // Final checks
+        System.out.println("Is the queue empty: " + arrq.isEmpty());
+        System.out.println("Queue size: " + arrq.size());
     }
 }
