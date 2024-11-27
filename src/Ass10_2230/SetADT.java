3c0ad15a646c3f2aaa47b0c412ded1da095a4038
Colton Isles <coltonisles@gmail.com>
2024-11-26 21:27:27 -0800

    Create SetADT.java

---
+++ src/Ass10_2230/SetADT.java
@@ -0,0 +1,14 @@
+package Ass10_2230;
+
+public abstract class SetADT {
+
+    public void add(int element) {
+
+    }
+    public void remove(int element) {
+        
+    }
+    public boolean contains(int element) {
+        return false; //temp
+    }
+}
